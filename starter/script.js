f9aef60b50c40072982e9f9b3cea29f288f1a0e7
randy918 <randy918@gmail.com>
2022-01-03 14:58:02 -0600

    completed Jason JS lesson 93

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const currentYear = getYear();
+const firstName = 'Oscar';
 
 const calcAge = birthYear => {
   const age = currentYear - birthYear;
+
+  const printAge = () => {
+    const output = `You are ${age} and born in ${birthYear}.`;
+    c(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      c(`You're a millennia!`);
+      const firstName = 'Steven';
+      c(firstName);
+    } else {
+      c(`You're NOT a millennial!`);
+    }
+    const add = (a, b) => a + b;
+    add(5, 7);
+
+    c(add(4, 8));
+  };
+  printAge();
+  c(firstName);
   return age;
 };
 
-c(calcAge(1959));
+c(calcAge(1985));
